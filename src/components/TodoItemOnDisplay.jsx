1e4406544374300b46f6298d5e8f63fdedbdaec6
Giao U <lesliegiao.u@gmail.com>
2024-07-02 19:50:57 -0500

    added complete checkmark feature

--- src/components/TodoItemOnDisplay.jsx
+++ src/components/TodoItemOnDisplay.jsx
@@ -1,13 +1,21 @@
 import Styles from "./todoitem.module.css";
 
-export default function TodoItemOnDisplay({ todo, todos, setTodos }) {
+export default function TodoItemOnDisplay({ todo, setTodo, todos, setTodos }) {
   function handleDelete(todo) {
     setTodos(todos.filter((item) => item !== todo)); //filter out the item that match todo
   }
+  function handleClick(name) {
+    const newArray = todos.map((item) =>
+      item.name === name ? { ...item, done: !item.done } : item
+    );
+    setTodos(newArray);
+    console.log(todos);
+  }
+  const isComplete = todo.done?Styles.complete:"";
   return (
     <div className={Styles.item}>
       <div className={Styles.itemName}>
-        {todo.name}
+        <span className={isComplete} onClick={() => handleClick(todo.name)}>{todo.name}</span>
         <span>
           <button
             onClick={() => handleDelete(todo)}
