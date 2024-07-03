0853b2ce92e88c9741e6539d95b2667f0e59b4d8
Giao U <lesliegiao.u@gmail.com>
2024-07-02 20:45:19 -0500

    Added sorting function to push finished tasks back to the bottom.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,9 +2,12 @@ import TodoItemOnDisplay from "./TodoItemOnDisplay";
 import Styles from "./todolist.module.css";
 
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={Styles.list}>
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItemOnDisplay
           key={item.name}
           todo={item}
